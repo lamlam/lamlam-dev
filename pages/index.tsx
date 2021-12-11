d9ad65e6e15ec62203ed7a62adb4aa3cd9c23e76
lamlam <lamlam4r@gmail.com>
2021-12-11 22:26:49 +0900

    Init

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Heading } from '@chakra-ui/react';
+import { ReactElement } from 'react';
+export default function Home(): ReactElement {
+  return (
+    <div>
+      <Heading>Hello world</Heading>
+    </div>
+  );
+}
